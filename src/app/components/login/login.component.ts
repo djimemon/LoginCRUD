061cdc2dae146e15dc558bd124fa41e7c6836aaa
David <djimemon@emeal.nttdata.com>
2022-04-25 16:59:13 +0200

    base login check

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user";
+import { LoginService } from "../../services/login/login.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  users: User[] = [];
+  username: String;
+  password: String;
+  constructor(private loginService: LoginService) { }
 
-  constructor() { }
+  getUsers(): void{
+    this.users = this.loginService.getUsers();
+  }
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  checkUser(): boolean{
+
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.username==this.users[i].name&&this.password==this.users[i].password) { //TODO añadir al localstorage y validacion
+        console.log("USUARIO CORRECTO")
+        return true
+      }
+    }
+    console.log("USUARIO INCORRECTO")
+    return false
   }
 
 }
--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {User} from "../../models/user";
+import { USERS } from "../../datasets/mock-users";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
+  getUsers(): User[]{
+    return USERS;
+  }
   constructor() { }
 }
